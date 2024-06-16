06373803656c11151367a007fae55da6ec183eee
Christopher Trejo <cjt0060@auburn.edu>
2024-06-15 20:57:31 -0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Christopher Joseph on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
